566759d8380e534f9dcb03454c1f5211db2d43a9
KaykyDeSouzaDias <kaykydias871@gmail.com>
2025-11-29 17:25:48 -0300

    Feat: Estruturação do frontend com integração incial da api da open weather

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  getWeather()
+})
+
+async function getWeather() {
+  try {
+    const response = await fetch('/api/weather')
+    const data = await response.json()
+    console.log(data)
+  } catch (error) {
+    console.error('Erro ao buscar clima:', error)
+  }
+}
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
