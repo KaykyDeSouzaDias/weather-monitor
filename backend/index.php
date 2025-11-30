7e5bbbbd9755109b2e3098757214dd5154ba9a38
KaykyDeSouzaDias <kaykydias871@gmail.com>
2025-11-30 01:28:55 -0300

    Feat: Implementação de parametros de latitude/longitude para recebimento de valores dinamicos

--- backend/index.php
+++ backend/index.php
@@ -3,15 +3,24 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 $apiKey = getenv('OPENWEATHER_API_KEY');
-$latitude = 22.54;
-$longitude = 43.10;
+$latitude = $_GET['lat'] ?? null;
+$longitude = $_GET['lon'] ?? null;
 
 if (!$apiKey) {
+    http_response_code(400);
     echo json_encode(["error" => "API Key não configurada"]);
     exit;
 }
+if (!$latitude || !$longitude) {
+    http_response_code(400);
+    echo json_encode(["error" => "Latitude e Longitude são obrigatórias"]);
+    exit;
+}
 
-$apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat={$latitude}&lon={$longitude}&appid={$apiKey}";
+$latitudeFormatted = htmlspecialchars($latitude);
+$longitudeFormatted = htmlspecialchars($longitude);
+
+$apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat={$latitudeFormatted}&lon={$longitudeFormatted}&appid={$apiKey}";
 
 $response = file_get_contents($apiUrl);
 
