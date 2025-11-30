680afdd575c83967e8c41befeca89efc5063169b
KaykyDeSouzaDias <kaykydias871@gmail.com>
2025-11-29 23:06:49 -0300

    Feat: Estilização da visualização de dados principais no dashboard com imagem de fundo conforme weather

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import AppSidebar from '@/components//layout/AppSideBar.vue'
 import { SidebarProvider, SidebarTrigger, SidebarInset } from '@/components/ui/sidebar'
+import { Card, CardContent } from '@/components/ui/card'
 import { onMounted, ref } from 'vue'
 import type { IWeather } from './models/weather'
+import { weatherBackgroundSelector } from './helpers/weather-bg-selector'
 
 const weather = ref<IWeather>()
 
@@ -24,7 +26,12 @@ async function getWeather() {
 <template>
   <SidebarProvider :style="{ '--sidebar-width': '25rem' }">
     <SidebarInset>
-      <div class="w-full h-screen">
+      <div
+        :class="`w-full h-screen bg-no-repeat bg-cover`"
+        :style="{
+          backgroundImage: `url('${weatherBackgroundSelector(weather?.weather[0]?.icon ?? '')}')`,
+        }"
+      >
         <div class="w-full h-full flex flex-col">
           <header class="h-16 flex items-center gap-2 px-4">
             <span class="text-md font-bold"> weather.monitor </span>
@@ -32,17 +39,31 @@ async function getWeather() {
           </header>
 
           <div class="w-full h-full flex items-end p-16">
-            <div class="flex items-center gap-6">
-              <span class="text-9xl font-medium">{{ weather?.main.temp.toFixed(0) }}°</span>
-              <div class="flex flex-col">
-                <span class="text-5xl font-medium">{{ weather?.name }}</span>
-                <span class="text-md font-medium">{{ new Date().toString().slice(0, 25) }}</span>
-              </div>
-              <div class="flex flex-col items-center">
-                <img :src="`https://openweathermap.org/img/wn/${weather?.weather[0]?.icon}@2x.png`"></img>
-                <span class="text-md font-medium">{{ weather?.weather[0]?.main }}</span>
-              </div>
-            </div>
+            <Card
+              class="bg-gray-500 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-white shadow-lg/50"
+            >
+              <CardContent>
+                <div class="flex items-center gap-6">
+                  <span class="text-9xl font-medium text-white"
+                    >{{ weather?.main.temp.toFixed(0) }}°</span
+                  >
+                  <div class="flex flex-col">
+                    <span class="text-5xl font-medium text-white">{{ weather?.name }}</span>
+                    <span class="text-md font-medium text-white">{{
+                      new Date().toString().slice(0, 25)
+                    }}</span>
+                  </div>
+                  <div class="flex flex-col items-center">
+                    <img
+                      :src="`https://openweathermap.org/img/wn/${weather?.weather[0]?.icon}@2x.png`"
+                    />
+                    <span class="text-md font-medium text-white capitalize">{{
+                      weather?.weather[0]?.description
+                    }}</span>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
           </div>
         </div>
       </div>
